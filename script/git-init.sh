d7e62ebca012df8a07716ce0ea240c5c1a6b8f52
amairovi <andrei.mairovich@electrolux.com>
2020-02-18 17:44:06 +0300

    Add checks during git-init.sh

--- script/git-init.sh
+++ script/git-init.sh
@@ -1,4 +1,27 @@
 cd ..
 
-git config --global core.excludesFile $PWD/git/global_ignore
-git config --global --add include.path $PWD/git/config
+excludesfile=$(git config --list | grep 'core.excludesfile' | head -n 1)
+
+if [[ -z "$excludesfile" ]]
+then
+	echo 'core.excludesfile is not initialised. Initialising...'
+
+	git config --global core.excludesFile $PWD/git/global_ignore
+
+	echo 'core.excludesfile was successfully initialised'
+else
+	echo 'core.excludesfile is already initialised'
+fi
+
+include_my_config_string=$(git config --list | grep '/git/config' | head -n 1)
+
+if [[ -z "$include_my_config_string" ]]
+then
+	echo 'include.path is not initialised with my custom configuration. Initialising...'
+
+	git config --global --add include.path $PWD/git/config
+
+	echo 'include.path was successfully initialised with my custom configuration'
+else
+	echo 'include.path is already initialised with my custom configuration'
+fi
